59244ec66c63503807716de32d41da48906404af
voroninaa09 <80808656+voroninaa09@users.noreply.github.com>
2021-03-31 11:34:25 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var a;
 var b;
 var c;
 
+
 while (t) {
  a = Number(prompt( "Введите переменную a"));
  b = Number(prompt( "Введите переменную b"));
@@ -17,26 +18,46 @@ else {
 	alert ("Попробуйте снова");
 }
 }
-
 let D = b*b-4*a*c;
+if (a==0 && b==0 && c!=0){
+	alert ("Уравнение неверно")
+}
+else
+if(a==0 && b==0 && c==0){
+let x1 = 0;
+alert ("x= " + x1);
+}
 
-if (D==0) {
-	let sqrD = Math.sqrt(D);
-	let x1 = (-b)/2*a;
-	alert(x1);
+else
+if(a==0 && b!=0 && c==0){
+let x1 = 0;
+alert ("x= " + x1);
 }
-else if (D>0){
-	let sqrD = Math.sqrt(D);
-	let x1 =((-b)+sqrD)/2*a;
-	let x2 =((-b)-sqrD)/2*a;
-	alert(x1 + " "+x2);
+
+else
+if(a==0 && b!=0 && c!=0){
+let x1 = -(c/b);
+alert ("x= " + x1);
 }
-else {
+
+
+else if (D == 0) {
+	let sqrD = Math.sqrt(D);
+	let x1 = ((-b)/(2*a)).toFixed(2);
+	alert("x= " + x1);
+}
+else if (D > 0){
+	let sqrD = Math.sqrt(D);
+	let x1 =(((-b)+sqrD)/(2*a)).toFixed(2);
+	let x2 =(((-b)-sqrD)/(2*a)).toFixed(2);
+	alert("x1= " + x1 + " " + "x2= " + x2);
+}
+else if (D < 0) {
 	console.log("a" + a);
 	console.log("b" + b);
 	console.log("c" + c);
 	let sqrD = Math.sqrt(D*(-1));
-	let x1 =((-b)/2*a).toFixed(2) + "+" + (sqrD/2*a).toFixed(2) + "i";
-	let x2 =((-b)/2*a).toFixed(2) + "-" + (sqrD/2*a).toFixed(2) + "i";
-	alert(x1 + " "+x2);
+	let x1 =((-b)/(2*a)).toFixed(2) + "+" + (sqrD/(2*a)).toFixed(2) + "i";
+	let x2 =((-b)/(2*a)).toFixed(2) + "-" + (sqrD/(2*a)).toFixed(2) + "i";
+	alert("x1= " + x1 + " " + "x2= " + x2);
 }
